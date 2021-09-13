f60f5378100b7e812841779a830f46fd22abd256
juri.gontard <juri.gontard@gwdg.de>
2021-09-13 14:33:58 +0200

    Adding camera Plug-In

--- www/js/index.js
+++ www/js/index.js
@@ -24,7 +24,24 @@ document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
-
+    
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
+    document.getElementById("cameraTakePicture").addEventListener ("click", cameraTakePicture);
+}
+
+function cameraTakePicture() {
+   navigator.camera.getPicture(onSuccess, onFail, {
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+   });
+   
+   function onSuccess(imageData) {
+      var image = document.getElementById('myImage');
+      image.src = "data:image/jpeg;base64," + imageData;
+   }
+   
+   function onFail(message) {
+      alert('Failed because: ' + message);
+   }
 }
